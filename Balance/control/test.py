995df1a7591afa3759c703f440dc3d2f60ac3f6e
Vincenthng <vincent_gameboy@hotmail.com>
2016-07-25 14:41:48 +0800

    new

---
+++ Balance/control/test.py
@@ -0,0 +1,9 @@
+from  generating.linear_recom import *
+from  basic_method.add_data import *
+linear_recom()
+
+
+rate=0.15
+
+
+add_data(rate)
--- Balance/generating/linear_recom.py
+++ Balance/generating/linear_recom.py
@@ -1,3 +1,5 @@
+import random
+from numpy import *
 def linear_recom(vector1,vector2):
     r = random.uniform(0.3,0.7)
     new_bug = []
