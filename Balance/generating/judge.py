67b1db385c80cbaa8b7af6197f1bd9448257356b
Christopherleikuan <1434262493@qq.com>
2016-07-28 00:02:12 +0800

    Create judge.py

---
+++ Balance/generating/judge.py
@@ -0,0 +1,16 @@
+# coding=UTF-8
+from basic_method import MORPH
+'''在各种算法子中使用的lable生成函数，son的距离跟父母谁近那么lable就跟谁
+    本来想着是直接添加的但是array不支持 + 重载，于是只好直接修改'''
+def judge_lable(vector1,vector2,vector_son):
+    if MORPH.get_deances(vector1,vector_son) > MORPH.get_deances(vector2,vector_son): #儿子跟vector2姓
+        v2_len = len(vector2)
+        vson_len = len(vector_son)
+        vector_son[vson_len] = vector2[v2_len]
+    else:
+        v1_len = len(vector1)
+        vson_len = len(vector_son)
+        vector_son[vson_len] = vector1[v1_len]
+    return vector_son
+
+print "judge:\n",judge_lable(MORPH.all_inputs_vector[2],MORPH.all_inputs_vector[3],MORPH.all_inputs_vector[4])
