1eb71f4f57c962c1dfd50025e2a423942dc7e7c9
Christopherleikuan <1434262493@qq.com>
2016-07-31 01:13:43 +0800

    Update single_point.py

--- Balance/generating/single_point.py
+++ Balance/generating/single_point.py
@@ -9,7 +9,7 @@ def single_point(vector1,vector2):
     print random_point
     new_bug_1 = []
     new_bug_2 = []
-    new_bug_point = []
+    new_bug = []
     array(new_bug_1)
     array(new_bug_2)
     new_bug_1 = vector1[0:random_point]
@@ -21,8 +21,11 @@ def single_point(vector1,vector2):
     new_bug_2 = list(new_bug_2)
 
 
-    new_bug_point= new_bug_1+new_bug_2
-    print new_bug_point
-    array(new_bug_point)
+    new_bug = new_bug_1 + new_bug_2
+    print new_bug
+    array(new_bug)
+    from judge import judge_lable
+    new_bug = judge_lable(vector1,vector2,new_bug)
 
-    return new_bug_point
+
+    return new_bug
