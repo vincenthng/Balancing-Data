b0bac71e09ce9ddab01de16fe796ad1c521f1f99
Vincenthng <vincent_gameboy@hotmail.com>
2016-08-21 11:08:00 +0800

    new

--- Balance/model/NaiveBayes.py
+++ Balance/model/NaiveBayes.py
@@ -5,36 +5,47 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import classification_report
 
-def naivebayes(filename1,filename2):
-    read_file(filename1)
-    train=convert(listCsv[1:])
+def naivebayes(file , outputfile):
+    fn = []
 
-    read_file(filename2)
-    test=convert(listCsv[1:])
+    import glob
+    for filename in glob.glob(r''+file+'/*.csv'):
+        fn.append(filename)
 
-    for i in test:
-        if i[29]>0:
-            i[29]=1
+    print fn
 
-    for i in train:
-        if i[29]>0:
-            i[29]=1
+    for j in range(0, len(fn) - 1):
+
+        read_file(fn[j])
+        train=convert(listCsv[1:])
+
+        read_file(fn[j+1])
+        test=convert(listCsv[1:])
+
+        for i in test:
+            if i[29]>0:
+                i[29]=1
+
+        for i in train:
+            if i[29]>0:
+                i[29]=1
+
+        clf = MultinomialNB().fit(train[:,:29], train[:,29])
+        doc_class_predicted = clf.predict(test[:,:29])
+
+        print(mean(doc_class_predicted == test[:,29]))
 
 
-    clf = MultinomialNB().fit(train[:,:29], train[:,29])
-    doc_class_predicted = clf.predict(test[:,:29])
+        precision, recall, thresholds = precision_recall_curve(test[:,29], clf.predict(test[:,:29]))
+        answer = clf.predict_proba(test[:,:29])[:, 1]
+        report = answer > 0.5
+        print(classification_report(test[:,29], report, target_names=['neg', 'pos']))
 
-    print(mean(doc_class_predicted == test[:,29]))
+        out = open(outputfile, "a")
+        out.write(fn[j]+"->"+fn[j+1]+"\n"+"\n")
+        out.write(classification_report(test[:,29], report, target_names=['neg', 'pos']))
+        out.write("\n")
+        out.close()
 
 
-    precision, recall, thresholds = precision_recall_curve(test[:,29], clf.predict(test[:,:29]))
-    answer = clf.predict_proba(test[:,:29])[:, 1]
-    report = answer > 0.5
-    print(classification_report(test[:,29], report, target_names=['neg', 'pos']))
-
-fn=[]
-
-import glob
-for filename in glob.glob(r'lang/*.csv'):
-    fn.append(filename)
-
+naivebayes("lang","result.txt")
